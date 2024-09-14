c38e51dc5fe64233160a845d08bf322803f0c7c7
whyismeleige <piyushjain31456@gmail.com>
2024-09-14 20:02:23 +0530

    COmmit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('Btn').addEventListener('click',function(){
+    window.location.href = 'https://www.instagram.com/powerbytes.sjc/';
+})
+
+document.getElementById('rulesButton').addEventListener('click',function(){
+    window.location.href = 'https://forms.gle/gHQDLBUGWchdd9sF8';
+});
